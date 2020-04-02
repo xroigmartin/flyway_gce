f0f4f983b9cc96bf7de621f73b3975e2ceb909c6
xroigmartin <xroigmartin@gmail.com>
2020-04-02 16:45:47 +0200

    refactor: Change name of index

--- src/main/resources/db/migration/V1.1__Create_table_domain.sql
+++ src/main/resources/db/migration/V1.1__Create_table_domain.sql
@@ -3,11 +3,11 @@ CREATE TABLE IF NOT EXISTS gce.domain (
 	code_domain VARCHAR(10) NOT NULL COMMENT 'Code of domain',
 	description VARCHAR(50) COMMENT 'Description of domain',
 	enable BOOLEAN default TRUE NOT NULL COMMENT 'Can use the domain',
-   CONSTRAINT
+    CONSTRAINT 
 		PRIMARY KEY (id),
-	CONSTRAINT 
+	CONSTRAINT code_domain_uk
 		UNIQUE (code_domain),
-	INDEX dominio_codigo_idx (code_domain)
+	INDEX code_domain_idx (code_domain)
 );
 
 CREATE SEQUENCE gce.domain_seq START WITH 1 INCREMENT BY 1;
